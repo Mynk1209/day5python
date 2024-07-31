2034e43e91f881e256d751527045be3840a5c7b6
Mayank Tyagi <mayank.morta1209@gmail.com>
2024-07-31 14:37:55 +0530

    summer internship

---
+++ day5.py
@@ -0,0 +1,77 @@
+# num1 = int(input('Plz enter first number : '))
+# num2 = int(input('Plz enter second number : '))
+# output = num1 % num2 
+# print (output)
+# num1 = int(num1)
+# num2 = int(num2)
+
+# print(type(num1))
+# print(type(num2))
+# num1 = int(input('Plz enter first number : '))
+# num2 = int(input('Plz enter second number : '))
+# output = num1 ** num2
+# print (output)
+# num1 = int(input("enter first number :"))
+# num1 +=1
+# num1 = num1 +2
+# print(num1)
+
+# num1 = int(input('Enter first number : '))
+# num2 = int(input(' Enter second number : '))
+# # print(num1 == num2)
+# print(num1 != num2)
+# print (num1 > num2)
+# print (num1<= num2)
+# num1 = int(input('Enter first number : '))
+# num2 = int(input(' Enter second number : '))
+# num3 = int (input("enter third number :"))
+# if  num1 > num2 :
+#     print(' num1 is grater then ')
+# elif num2 > num3:
+#      print('num2 is grater then')
+# else:
+#     print('num3 is grater thane ')
+
+# num1 = int(input('Enter first number : '))
+# num2 = int(input(' Enter second number : '))
+# num3 = int (input("enter third number :")) 
+
+# if num1 > num2  and num1 > num3: 
+#     print ('num 1 is bigest ')
+
+# elif num2 > num1 and num2 > num3:
+#     print ("number2 is bigest")
+# else:
+#     print("num3 is bigest")
+
+    # # and gate truth table
+    # A   B    RESULT
+    # 1   0      0
+    # 0   1      0
+    # 0   0      0
+    # 1   1      1
+
+
+     # # or gate truth table
+    # A   B  =  RESULT
+    # 1   0  =    1
+    # 0   1  =    1
+    # 0   0  =    0
+    # 1   1  =    1
+# age= int(input("plz enter youe age  =="))
+# # age = 15
+# if age > 18 and age<100 :
+#     print ("you can vote")
+# else:
+#     print("you cant vote")    
+    
+# student_list= ["jugnu","mayank","rahul","jack"]
+# if "jack" in student_list:
+#     print('present')
+# else:
+#     print("not present")
+
+
+
+
+
